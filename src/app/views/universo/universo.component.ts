95d4d53a7f869cb7afeb915b48104fa6a0bdcfa3
JuanAnayaSanchez <167732186+JuanAnayaSanchez@users.noreply.github.com>
2025-09-16 15:23:53 -0500

    Creacion componente universo

---
+++ src/app/views/universo/universo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MainMenuComponent } from '../../shared/main-menu/main-menu.component';
+
+@Component({
+  selector: 'app-universo',
+  imports: [MainMenuComponent],
+  templateUrl: './universo.component.html',
+  styleUrl: './universo.component.css'
+})
+export class UniversoComponent {
+
+}
